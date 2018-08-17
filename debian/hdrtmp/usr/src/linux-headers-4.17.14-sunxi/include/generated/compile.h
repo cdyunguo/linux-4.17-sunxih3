aa2ad849e492d0fbf45abd605d33983644e86fa5
jianghong-023 <jianghong_023@163.com>
2018-08-16 22:32:58 -0700

    linux cedrus

---
+++ debian/hdrtmp/usr/src/linux-headers-4.17.14-sunxi/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP Tue Jul 29 22:37:20 CST 2025"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "Ubuntu-Compile"
+#define LINUX_COMPILER "gcc version 7.2.1 20171011 (Linaro GCC 7.2-2017.11)"
